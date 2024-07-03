8839bf9a096dfacd8234844312ef5ec78feea743
Davide-Morellato <morellato.davide@yahoo.it>
2024-07-03 18:46:06 +0200

    ADD: filter checkbox

--- src/pages/AppHomepage.vue
+++ src/pages/AppHomepage.vue
@@ -7,29 +7,45 @@ export default {
     return {
       restaurants: [], //array vuoto da popolare con la chiamata axios
       types: [], //array vuoto da popolare con la chiamata axios
+
+      type_ids: [], //salvare gli ids delle checkbox
     };
   },
 
   methods: {
     //recupero i ristoranti tramite l'api
     fetchRestaurants() {
-      axios.get("http://127.0.0.1:8000/api/restaurants").then((res) => {
-        // console.log(res.data.restaurants);
-        
-        this.restaurants = res.data.restaurants; //popolo l'array dei ristoranti
-      });
+      const params = {};
+      if (this.type_ids.length) {
+        params.types = this.type_ids.join(",");
+      }
+      axios
+        .get("http://127.0.0.1:8000/api/restaurants", { params })
+        .then((res) => {
+          //   console.log(res.data.restaurants);
+
+          this.restaurants = res.data.restaurants; //popolo l'array dei ristoranti
+        });
     },
-    //rescupero le tipologie tramite l'api
+    //recupero le tipologie tramite l'api
     fetchTypes() {
       axios.get("http://127.0.0.1:8000/api/types").then((res) => {
-        
+        // console.log(res.data.types);
+
         this.types = res.data.types; //popolo l'array delle tipologie
       });
+    },
+
+    // fitered restaurant
+    getIdType(index) {
+      this.type_ids.push(index);
+      //   console.log("n. " + index);
+      //   console.log(this.type_ids);
     },
   },
   created() {
     //richiamo le funzioni dei methods per la stampa in pagina
-    this.fetchRestaurants();
+    // this.fetchRestaurants();
     this.fetchTypes();
   },
 };
@@ -39,27 +55,20 @@ export default {
   <main>
     <section class="h-100">
       <div class="container py-5 h-100">
-        <div
-          class="row aling-items-center flex-column justify-content-center h-100"
-        >
-          <div class="col-auto text-center">
-            <!-- <h1>Hello Vue <font-awesome-icon :icon="['fab', 'vuejs']" /></h1> -->
-            <div v-for="restaurant in restaurants" :key="restaurant.id">
-              <h1>
-                {{ restaurant.name }}
-              </h1>
-            </div>
+        <div>
+          <h1>Ristoranti</h1>
+          <div>
+            <label v-for="type in types" :key="type">
+              <input type="checkbox" :value="type" v-model="type_ids" />
+              {{ type.name }}
+            </label>
           </div>
-
-          <div class="col-auto text-center">
-            <!-- <h1>Hello Vue <font-awesome-icon :icon="['fab', 'vuejs']" /></h1> -->
-            <div v-for="type in types" :key="type.id">
-              <input type="checkbox" :value="type.id" name="type" id="type"/>
-              <label for="type">
-                {{ type.name }}
-              </label>
-            </div>
-          </div>
+          <button @click="fetchRestaurants()">Filtra Ristoranti</button>
+          <ul>
+            <li v-for="restaurant in restaurants" :key="restaurant.id">
+              {{ restaurant.name }} - {{ restaurant.slug }}
+            </li>
+          </ul>
         </div>
       </div>
     </section>
