4ed3fcea0375763ad3d1d9ad65e6416fc0eb0c54
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-04 16:58:18 +0200

    ADD: content on AppAdmin

--- src/pages/AppAdmin.vue
+++ src/pages/AppAdmin.vue
@@ -1,7 +1,12 @@
 <template>
-<div class="container">
-    <div class="row">
-        ADMIN
+<div class="container py-5">
+    <div class="row py-5">
+        <div class="col py-5">
+            <h1>DELIVEBOO ADMIN</h1>
+            <p class="py-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quam nulla animi possimus rem assumenda! Voluptatibus laudantium perferendis vero voluptas ipsum iste ducimus architecto fugit culpa, inventore, modi tempora eveniet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae cumque, officia quibusdam ut cupiditate nihil consequuntur a voluptatum adipisci nostrum eius corporis assumenda ea, distinctio odio repellat vitae possimus necessitatibus.
+            </p>
+        </div>
     </div>
 </div>
 </template>
