47408c9fb7d1fb4a09b145b8b5f4c808c1539b8d
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-04 16:58:08 +0200

    ADD: img on AppCard

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,6 +1,7 @@
 <template>
-<div class="card p-2">
+<div class="card p-5">
     <h3 class="text-center">{{ restaurant.name }}</h3>
+    <img class="restaurant-img mt-3" :src="imgPath + restaurant.thumb" alt="Foto Ristorante">
 </div>
 </template>
 
@@ -8,10 +9,19 @@
     export default {
         props:{
             restaurant: Object
-        }
+        },
+        data() {
+            return {
+            imgPath: '../../public/restaurants/'
+            };
+        },
     }
 </script>
 
 <style lang="scss" scoped>
+.restaurant-img{
+    aspect-ratio: 1/1;
+    object-fit: cover;
+}
 
 </style>
