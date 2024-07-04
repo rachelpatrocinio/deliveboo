3b683687871d8f81d7dcf8ba1e463fcefd7dd08d
Marcomagnano93 <marcog.magnano@gmail.com>
2024-07-04 18:16:17 +0200

    ADD: menu page

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,13 +1,26 @@
 <template>
-<div class="card p-2">
+<div class="card p-2"> 
     <h3 class="text-center">{{ restaurant.name }}</h3>
+    <div>
+        <ul>
+            <li v-for="type in restaurant.types">
+                {{ type.name }}
+            </li>
+        </ul>
+    </div>
+    <div class="text-end">
+        <button class="btn btn-info">
+            <RouterLink class="link" :to="{name: 'menu', params: {slug: 'show.slug'}}">Menu</RouterLink>
+        </button>
+    </div>
 </div>
 </template>
 
 <script>
     export default {
         props:{
-            restaurant: Object
+            restaurant: Object,
+            slug: {type:Object},
         }
     }
 </script>
--- src/pages/AppMenu.vue
+++ src/pages/AppMenu.vue
@@ -1,20 +1,40 @@
 <template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card"
+                v-for="dish in dishes"
+                >
+                    <div class="card-title">
+                        {{ dish.name }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </template>
 <script>
+import axios from "axios";
     export default {
         data() {
             return {
-                dishes: [] //array dei piatti
+                dishes: [], //array dei piatti
+                restaurant: [] //singolo ristorante
             }
         },
         methods: {
-            fetchDishes() {
+            fetchDishes() { // Popola l'array dei piatti
                 axios.get("http://127.0.0.1:8000/api/dishes")
                     .then((res) => {
-                this.dishes = res.data.dishes; // Popola l'array dei piatti
+                this.dishes = res.data.dishes; 
+                console.log(res.data.dishes);
             })
-        }
+
+        },
+            fetchRestaurant() { // chiamata parametrica del singolo ristorante
+                axios.get("http://127.0.0.1:8000/api/restaurants/" + this.$route.params.slug).then((res)=>{this.restaurant = res.data.restaurant})
+                }
     },
     created() {
         this.fetchDishes();
