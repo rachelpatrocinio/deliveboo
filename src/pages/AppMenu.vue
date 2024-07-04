5276dc00550e94fff3981ff0bd513301afb3ecdd
Marcomagnano93 <marcog.magnano@gmail.com>
2024-07-04 16:16:17 +0200

    ADD: router AppMenu

---
+++ src/pages/AppMenu.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                dishes: [] //array dei piatti
+            }
+        },
+        methods: {
+            fetchDishes() {
+                axios.get("http://127.0.0.1:8000/api/dishes")
+                    .then((res) => {
+                this.dishes = res.data.dishes; // Popola l'array dei piatti
+            })
+        }
+    },
+    created() {
+        this.fetchDishes();
+    }
+}
+    </script>
+    
+    <style lang="scss" scoped>
+    
+    </style>
--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import AppHomepage from './pages/AppHomepage.vue'
 import AppAdmin from './pages/AppAdmin.vue'
+import AppMenu from './pages/AppMenu.vue'
 
 
 
 const routes = [
   { path: '/', component: AppHomepage },
-  { path: '/admin', component: AppAdmin }
+  { path: '/admin', component: AppAdmin },
+  { path: '/menu/:slug', component: AppMenu }
 //   { path: '/portfolio', component: AppPortfolio },
 //   { path: '/portfolio/:slug', name: 'project.show' , component: AppProject},
 //   { path: '/contact', component: AppContact },
