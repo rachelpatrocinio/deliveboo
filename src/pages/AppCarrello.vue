2b7a18c919c71fe035560d463822b8106122ca0b
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-05 16:53:05 +0200

    ADD: AppCarrello content

---
+++ src/pages/AppCarrello.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container">
+    <div class="row">
+        <h1>CARRELLO</h1>
+        <h3 v-for="dish in store.chart"> {{dish.name}} </h3>
+    </div>
+</div>
+</template>
+
+<script>
+import {store} from '../store.js';
+
+export default {
+    data(){
+        return{
+            store
+        }
+    },
+    mounted(){
+        console.log(this.store.chart)
+    }
+}
+        
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
