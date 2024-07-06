a8d0c0be3e9a48b712efb0948ac9adafec3ee3f5
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-06 11:06:46 +0200

    ADD: managed cart

--- src/pages/AppCarrello.vue
+++ src/pages/AppCarrello.vue
@@ -1,8 +1,27 @@
 <template>
-<div class="container">
-    <div class="row">
-        <h1>CARRELLO</h1>
-        <h3 v-for="dish in store.chart"> {{dish.name}} </h3>
+<div class="container py-5">
+    <div class="row py-5">
+        <h1 class="p-0">CARRELLO</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th> Piatto </th>
+                    <th> Prezzo </th>
+                    <th> Elimina</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="dish in store.chart">
+                    <td>{{dish.name}}</td>
+                    <td>{{dish.price}}</td>
+                    <td>
+                        <button class="btn delete-button" @click="deleteDish(dish)">
+                            <img src="../../public/icons/bin.png" alt="Eliminazione">
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 </template>
@@ -21,6 +40,21 @@ export default {
         //funzione per il salvataggio del carrello nel localStorage
         saveChart() {
             localStorage.chart = JSON.stringify(this.store.chart);
+        },
+        deleteDish(dish){
+            console.log('delete dish', dish.name);
+            this.store.chart.splice(dish, 1);
+            localStorage.chart = JSON.stringify(this.store.chart);
+            console.log(localStorage.chart); 
+
+            // localStorage.removeItem(dish); // qui ritornano anche quando cambio rotta
+            // localStorage.chart = localStorage.removeItem(dish); // si blocca tutto
+
+            // const storage = localStorage.chart
+            // storage.removeItem(dish); // non so perchè ma mi dice is not a function
+
+            // localStorage.removeItem(localStorage.chart[dish]); // undefiened index
+            // console.log(localStorage)
         }
     },
 
@@ -36,5 +70,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 </style>
