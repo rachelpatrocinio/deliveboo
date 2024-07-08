a4c4bd6cb03de2317935ded12f985141f3f2b306
gab <corbanigabriele@gmail.com>
2024-07-08 10:04:38 +0200

    route

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import AppHomepage from './pages/AppHomepage.vue'
 import AppAdmin from './pages/AppAdmin.vue'
 import AppMenu from './pages/AppMenu.vue'
 import AppCarrello from './pages/AppCarrello.vue'
+import AppPayment from './pages/AppPayment.vue'
 
 
 
@@ -11,8 +12,9 @@ const routes = [
   { path: '/', component: AppHomepage },
   { path: '/admin', component: AppAdmin },
   { path: '/menu/:slug', name: 'menu', component: AppMenu},
-  { path: '/carrello', name: 'carrello', component: AppCarrello}
+  { path: '/carrello', name: 'carrello', component: AppCarrello},
   //   { path: '/:pathMatch(.*)*', name:'not-found', component: AppNotFound},
+  { path: '/pagamento', name: 'pagamento', component: AppPayment},
 ]
 
 const router = createRouter({
