f8c1f376c8c1d5a20ace4538433a5cdf620f2586
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-11 19:03:17 +0200

    ADD: Footer content

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -14,12 +14,92 @@ export default {
 <template>
   <svg class="developer-svg wave-1hkxOo mt-5" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100" preserveAspectRatio="none"><path class="wavePath-haxJK1 animationPaused-2hZ4IO" d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z" fill="#212529"></path></svg>
   <footer class="bg-dark">
-    <div class="container py-5 text-center text-white">
-      <h1>FOOTER</h1>
+    <div class="container py-5 text-white">
+      <div class="row justify-content-between">
+        <div class="col-4">
+          <img class="px-2" src="../../../public/icons/facebook.png" alt="">
+          <img class="px-2" src="../../../public/icons/instagram.png" alt="">
+          <img class="px-2" src="../../../public/icons/youtube.png" alt="">
+        </div>
+        <div class="col-4 text-end">DeliveBoo S.p.a</div>
+        <hr class="mt-5">
+        <section class="d-flex text-center web-developers">
+          <div class="col">
+            <img class="developers" src="../../../public/developers/marco.jpeg" alt="">
+            <h6>Marco Magnano</h6>
+            <figure>
+              <a href="https://www.linkedin.com/in/marcomagnano93/" target="_blank">
+                <img src="../../../public/icons/linkedin.png" alt="LinkedIn">
+              </a>
+              <a href="https://github.com/Marcomagnano93" target="_blank">
+                <img src="../../../public/icons/github.png" alt="Github">
+              </a>
+            </figure>
+          </div>
+          <div class="col">
+            <img class="developers" src="../../../public/developers/davide.jpeg" alt="">
+            <h6>Davide Morellato</h6>
+            <figure>
+              <a href="https://www.linkedin.com/in/davide-m-4826ab177/" target="_blank">
+                <img src="../../../public/icons/linkedin.png" alt="LinkedIn">
+              </a>
+              <a href="https://github.com/Davide-Morellato" target="_blank">
+                <img src="../../../public/icons/github.png" alt="Github">
+              </a>
+            </figure>
+          </div>
+          <div class="col">
+            <img class="developers" src="../../../public/developers/rachel.jpeg" alt="">
+            <h6>Rachel A. Patrocinio</h6>
+            <figure>
+              <a href="https://www.linkedin.com/in/rachel-anne-patrocinio-80811b315/" target="_blank">
+                <img src="../../../public/icons/linkedin.png" alt="LinkedIn">
+              </a>
+              <a href="https://github.com/rachelpatrocinio" target="_blank">
+                <img src="../../../public/icons/github.png" alt="Github">
+              </a>
+            </figure>
+          </div>
+          <div class="col">
+            <img class="developers" src="../../../public/developers/gabriele.jpeg" alt="" target="_blank">
+            <h6>Gabriele Corbiani</h6>
+            <figure>
+              <a href="https://www.linkedin.com/in/gabriele-corbani-01a11a315/">
+                <img src="../../../public/icons/linkedin.png" alt="LinkedIn">
+              </a>
+              <a href="https://github.com/corgab" target="_blank">
+                <img src="../../../public/icons/github.png" alt="Github">
+              </a>
+            </figure>
+          </div>
+          <div class="col">
+            <img class="developers" src="../../../public/developers/fabio.jpeg" alt="" target="_blank">
+            <h6>Fabio Mazzilli</h6>
+            <figure>
+              <a href="https://www.linkedin.com/in/fabio-mazzilli-front-end-developer/">
+                <img src="../../../public/icons/linkedin.png" alt="LinkedIn">
+              </a>
+              <a href="https://github.com/fabiomazzilli85" target="_blank">
+                <img src="../../../public/icons/github.png" alt="Github">
+              </a>
+            </figure>
+          </div>
+        </section>
+      </div>
     </div>
   </footer>
-
 </template>
 
-<style>
+<style lang="scss" scoped>
+img.developers{
+  width: 80px;
+  border-radius: 99px;
+  margin: 10px 0;
+}
+
+figure{
+  img{
+    padding: 0 5px;
+  }
+}
 </style>
