11f1df81c68e5af8d0933299e93cfa4a6e43c14f
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-11 12:04:06 +0200

    REFACTOR: padding of AppHeroBanner

--- src/components/AppHeroBanner.vue
+++ src/components/AppHeroBanner.vue
@@ -2,7 +2,7 @@
 <section class="hero-banner mt-2">
     <div class="container">
         <div class="row py-5">
-            <div class="col-12 col-md-6 py-5">
+            <div class="col-12 col-md-6">
                 <h1 class="pt-5 title"> DELIVEBOO </h1>
                 <h4>Il tuo ristorante preferito, direttamente a casa tua!</h4>
                 <p class="pb-5"> Ordina dai migliori ristoranti della città e ricevi il tuo cibo preferito in pochi minuti. Consegniamo a domicilio piatti caldi e freschi, preparati con amore e cura dai migliori chef. Scopri nuove cucine, esplora menù variegati e goditi la comodità di un servizio rapido e affidabile!</p>
