1497fe488825ded3ba774ea5da7fa72be604bf93
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-12 18:01:52 +0200

    REFACTOR: AppContact padding

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -1,41 +1,43 @@
 <template>
   <div class="container py-5">
-    <!-- con il @submit.prevent, preveniamo l'invio del form -->
-    <form v-if="success === false" @submit.prevent="sendMessage" action="" >
-            <div class="mb-5">
-                <label for="name" class="form-label"><h3>NOME</h3></label>
-                <input type="text" class="form-control" id="name" v-model="name" placeholder="nome">
-                <div v-if="errors.name">
-                    <p class="text-danger">{{ errors.name.join(' ') }}</p>
-                </div>
-            </div>
-            <div class="mb-5">
-                <label for="email" class="form-label"><h3>EMAIL</h3></label>
-                <input type="email" class="form-control" id="email" v-model="email" placeholder="email@example.com">
-                <div v-if="errors.email">
-                    <p class="text-danger">{{ errors.email.join(' ') }}</p>
-                </div>
-            </div>
-            <div class="mb-3">
-                <label for="message" class="form-label"><h3>MESSAGGIO</h3></label>
-                <div class="form-floating">
-                    <textarea v-model="message" id="message" cols="30" rows="10" class="w-100 h-25 form-control"></textarea>
-                </div>
-                <div v-if="errors.message">
-                    <p class="text-danger">{{ errors.message.join(' ') }}</p>
-                </div>
-            </div>
-            <button class="btn btn-orange">INVIO</button>
-        </form>
+    <div class="row py-5">
+      <!-- con il @submit.prevent, preveniamo l'invio del form -->
+      <form v-if="success === false" @submit.prevent="sendMessage" action="" >
+              <div class="mb-5">
+                  <label for="name" class="form-label"><h3>NOME</h3></label>
+                  <input type="text" class="form-control" id="name" v-model="name" placeholder="nome">
+                  <div v-if="errors.name">
+                      <p class="text-danger">{{ errors.name.join(' ') }}</p>
+                  </div>
+              </div>
+              <div class="mb-5">
+                  <label for="email" class="form-label"><h3>EMAIL</h3></label>
+                  <input type="email" class="form-control" id="email" v-model="email" placeholder="email@example.com">
+                  <div v-if="errors.email">
+                      <p class="text-danger">{{ errors.email.join(' ') }}</p>
+                  </div>
+              </div>
+              <div class="mb-3">
+                  <label for="message" class="form-label"><h3>MESSAGGIO</h3></label>
+                  <div class="form-floating">
+                      <textarea v-model="message" id="message" cols="30" rows="10" class="w-100 h-25 form-control"></textarea>
+                  </div>
+                  <div v-if="errors.message">
+                      <p class="text-danger">{{ errors.message.join(' ') }}</p>
+                  </div>
+              </div>
+              <button class="btn btn-orange">INVIO</button>
+      </form>
 
-        <div v-else class="text-success py-5">
-            <h1>L'invio è andato a buon fine</h1>
-            <RouterLink to="/">
-              <button class="btn btn-orange mt-2">
-                Torna alla Homepage
-              </button>
-            </RouterLink>
-        </div>
+      <div v-else class="text-success py-5">
+          <h1>L'invio è andato a buon fine</h1>
+          <RouterLink to="/">
+            <button class="btn btn-orange mt-2">
+              Torna alla Homepage
+            </button>
+          </RouterLink>
+      </div>
+    </div>
   </div>
 </template>
 
