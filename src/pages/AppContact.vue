60572e21ec7b424bfa3817bd71b5587a9240e07a
gab <corbanigabriele@gmail.com>
2024-07-15 10:32:55 +0200

    .

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container py-5">
-    <div class="row py-5 d-flex flex-column align-items-center">
-      <div class="w-50 text-center py-3">
+    <div class="d-flex justify-content-center py-5 flex-sm-column">
+      <div class="text-center p-3 col-7 col-sm-auto">
         <h1>Contattaci</h1>
         <h5 class="mt-3">
           Benvenuto nella sezione contatti.
@@ -13,75 +13,59 @@
           Per ogni esigenza, noi di Deliveboo saremo a tua disposizione.
         </p>
         <p class="m-0">
-          Se hai riscontrato qualche problema e non sai come procedere, 
+          Se hai riscontrato qualche problema e non sai come procedere,
         </p>
         <p>
-          puoi compilare il seguente form, inserendo tutti i campi richiesti (<span class="text-danger fw-bold">*</span>).
+          puoi compilare il seguente form, inserendo tutti i campi richiesti (<span
+            class="text-danger fw-bold">*</span>).
         </p>
       </div>
       <!-- con il @submit.prevent, preveniamo l'invio del form -->
-      <form
-        v-if="success === false"
-        @submit.prevent="sendMessage"
-        action=""
-        class="w-50"
-      >
-        <div class="mb-5">
-          <label for="name" class="form-label"><h6 class="m-0">Nome <span class="text-danger fw-bold">*</span></h6></label>
-          <input
-            type="text"
-            class="form-control"
-            id="name"
-            v-model="name"
-            placeholder="nome cognome"
-          />
-          <div v-if="errors.name">
-            <p class="text-danger">{{ errors.name.join(" ") }}</p>
+      <div class="">
+        <form v-if="success === false" @submit.prevent="sendMessage" action="">
+          <div class="mb-5">
+            <label for="name" class="form-label">
+              <h6 class="m-0">Nome <span class="text-danger fw-bold">*</span></h6>
+            </label>
+            <input type="text" class="form-control" id="name" v-model="name" placeholder="nome cognome" />
+            <div v-if="errors.name">
+              <p class="text-danger">{{ errors.name.join(" ") }}</p>
+            </div>
           </div>
-        </div>
-        <div class="mb-5">
-          <label for="email" class="form-label"
-            ><h6 class="m-0">Email <span class="text-danger fw-bold">*</span></h6></label
-          >
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            v-model="email"
-            placeholder="email@example.com"
-          />
-          <div v-if="errors.email">
-            <p class="text-danger">{{ errors.email.join(" ") }}</p>
+          <div class="mb-5">
+            <label for="email" class="form-label">
+              <h6 class="m-0">Email <span class="text-danger fw-bold">*</span></h6>
+            </label>
+            <input type="email" class="form-control" id="email" v-model="email" placeholder="email@example.com" />
+            <div v-if="errors.email">
+              <p class="text-danger">{{ errors.email.join(" ") }}</p>
+            </div>
           </div>
-        </div>
-        <div class="mb-3">
-          <label for="message" class="form-label"
-            ><h6 class="m-0">Messaggio <span class="text-danger fw-bold">*</span></h6></label
-          >
-          <div class="form-floating">
-            <textarea
-              v-model="message"
-              id="message"
-              rows="5"
-              class="w-100 h-25 form-control"
-            ></textarea>
+          <div class="mb-3">
+            <label for="message" class="form-label">
+              <h6 class="m-0">Messaggio <span class="text-danger fw-bold">*</span></h6>
+            </label>
+            <div class="form-floating">
+              <textarea v-model="message" id="message" rows="5" class="w-100 h-25 form-control"></textarea>
+            </div>
+            <div v-if="errors.message">
+              <p class="text-danger">{{ errors.message.join(" ") }}</p>
+            </div>
           </div>
-          <div v-if="errors.message">
-            <p class="text-danger">{{ errors.message.join(" ") }}</p>
+          <div class="text-center">
+            <button class="btn btn-orange mt-4">INVIO</button>
           </div>
-        </div>
-        <div class="text-center">
-          <button class="btn btn-orange mt-4">INVIO</button>
-        </div>
-      </form>
+        </form>
 
-      <div v-else class="text-success py-5">
-        <h1>L'invio è andato a buon fine</h1>
-        <RouterLink to="/">
-          <button class="btn btn-orange mt-2">Torna alla Homepage</button>
-        </RouterLink>
+        <div v-else class="text-success py-5">
+          <h1>L'invio è andato a buon fine</h1>
+          <RouterLink to="/">
+            <button class="btn btn-orange mt-2">Torna alla Homepage</button>
+          </RouterLink>
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 
